69d78f5ac472b8ac1b6dd4f7d970edb0c8895904
Alex <ok.230111@gmail.com>
2026-08-05 00:19:09 +0200

    reset migrations: empty baseline

---
+++ migrations/versions/f518756c6e87_initial_schema.py
@@ -0,0 +1,24 @@
+"""initial schema
+
+Revision ID: f518756c6e87
+Revises: 
+Create Date: 2026-08-05 00:16:01.651562
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f518756c6e87'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
